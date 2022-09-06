21d27bf15fa883cc0beb2eaad8bc781e591ff9bb
KY-Lin22 <linkangyu1994@gmail.com>
2022-09-06 12:32:11 +0900

    sensitivity test

--- Example/AffineDVI/test_NIPOCPEC_Sensitivity.m
+++ Example/AffineDVI/test_NIPOCPEC_Sensitivity.m
@@ -1,34 +1,38 @@
 %% Solving OCPEC with NIPOCPEC solver
 clear all
 clc
+delete AffineDVI.gif
 delete Gen_InitialGuess.mat
 [~, ~, ~] = rmdir('autoGen_CodeFiles', 's');
 
-%% create an dynamics system
-timeStep = 0.02;
-plant = FilippovDI(timeStep);
+%% create an dynamics system 
+timeStep = 0.00001;
+plant = AffineDVI(timeStep);
+tau_Max = 2;
+tau_Min = -2;
+x_Max = [2; 2];
+x_Min = [-2; -2];
+plant.setDynVarLimit(tau_Max, tau_Min, x_Max, x_Min)
 plant.codeGen();
 
 %% formulate an OCPEC problem 
-nStages = 100;
-InitState = -1;
-
-StageCost.xRef = zeros(1, nStages);
+nStages = 100000;
+InitState = [-0.5; -1];
+StageCost.xRef = repmat([0; 0], 1, nStages);
 StageCost.tauRef = zeros(1, nStages);
-StageCost.xWeight = 2;
-StageCost.tauWeight = 0.00001; 
-TerminalCost.xRef = 5/3;
+StageCost.xWeight = [20; 20];
+StageCost.tauWeight = 1; 
+TerminalCost.xRef = [0; 0];
 TerminalCost.tauRef = 0;
-TerminalCost.xWeight = 2;
-TerminalCost.tauWeight = 0.00001;
+TerminalCost.xWeight = [20; 20];
+TerminalCost.tauWeight = 1;
+
+% show initial and reference configuration of given plant
+plant.plotConfiguration(InitState, TerminalCost.xRef)
 
 OCPEC = OCPEC_Perturbed(plant, timeStep, nStages, InitState, StageCost, TerminalCost, 'Reg_Scholtes');% 'SmoothingEquation', 'Reg_NCPs', 'Reg_Scholtes' 
 
-% set C
-C = OCPEC.x - (OCPEC.p(2) - OCPEC.p(1));
-OCPEC.setEqualityConstraints(C);
-
-% set G
+OCPEC.setEqualityConstraints([]);
 OCPEC.setInequalityConstraints([]);
 
 % generate necessary code files
@@ -59,6 +63,9 @@ solver.Option.zEnd  = 1e-4;
 solver.Option.sInit = 1e-1;
 solver.Option.sEnd  = 1e-4;
 
+InitState = [-0.5; -1];
+solver.OCPEC.InitState = InitState;
+
 % show solver information
 solver.showInfo();
 % generate initial guess
@@ -84,20 +91,24 @@ disp(['timeTest_iterations: ', num2str(timeTest_IterNum), '; ',...
 
 %% show result 
 plant.plotSimuResult(timeStep, InitState, solution.tau, solution.x, solution.p)
+plant.animateTrajectory(timeStep, InitState, solution.tau, solution.x, solution.p)
+solver.showResult(Info)
 
 %% solving OCPEC (sensitivity test)
-x0 = -1.9 : 0.02 : -0.9;
+x2 = -1.0 : 0.01 : -0.4;
 sEnd = [1e-1, 1e-2, 1e-3, 1e-4];
-num_x2 = size(x0, 2);
+num_x2 = size(x2, 2);
 num_sEnd = size(sEnd, 2);
 costRecord = zeros(num_sEnd, num_x2);
 
-Test_Num = 5;
+Test_Num = 10;
 solver.Option.printLevel = 0;
+
 for k = 1 : num_sEnd
     solver.Option.sEnd = sEnd(k);
+    
     for j = 1 : num_x2
-        InitState = x0(j);
+        InitState = [-0.5; x2(j)];
         solver.OCPEC.InitState = InitState;
         
         TestRecord.InitialGuess = cell(Test_Num, 1);
@@ -112,25 +123,27 @@ for k = 1 : num_sEnd
             Gen_InitialGuess = load('Gen_InitialGuess.mat');
             [solution, Info] = solver.solveOCPEC(Gen_InitialGuess.Iterate);
             TestRecord.InitialGuess{i, 1} = Gen_InitialGuess.Iterate;
-            TestRecord.solution{i, 1} = solution;          
+            TestRecord.solution{i, 1} = solution;           
             if Info.iterProcess.terminalStatus == 1
                 successCase = successCase + 1;
                 TestRecord.iterNum(successCase, 1) = Info.iterProcess.iterNum;
                 TestRecord.totalTime(successCase, 1) = Info.iterProcess.Time.total;
                 TestRecord.cost(successCase, 1) = Info.solutionMsg.totalCost;
             end
+            disp(['sEnd: ', num2str(sEnd(k))])
+            disp(['x_0(2): ', num2str(x2(j))])
             disp(['success / Test No.: ', num2str(successCase), ' / ', num2str(i)])
         end
         costRecord(k, j) = min(TestRecord.cost(1 : successCase, 1));
     end
-        
-end
 
+end
+%%
 figure(1)
-plot(x0, costRecord(1, :), 'k',...
-    x0, costRecord(2, :), 'b',...
-    x0, costRecord(3, :), 'r',...
-    x0, costRecord(4, :), 'g', 'LineWidth', 1.2)
+plot(x2, costRecord(1, :), 'k',...
+    x2, costRecord(2, :), 'b',...
+    x2, costRecord(3, :), 'r',...
+    x2, costRecord(4, :), 'g', 'LineWidth', 1.2)
 legend('1e-1', '1e-2', '1e-3', '1e-4')
-xlabel('x0')
+xlabel('x_0(2)')
 ylabel('cost')
