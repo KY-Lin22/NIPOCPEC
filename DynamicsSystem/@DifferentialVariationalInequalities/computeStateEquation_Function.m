c09e758f778cce2f54fff062876977ddbef9042a
KY-Lin22 <linkangyu1994@gmail.com>
2022-11-22 22:02:56 +0900

    adding the third computation method for state equation

--- DynamicsSystem/@DifferentialVariationalInequalities/computeStateEquation_Function.m
+++ DynamicsSystem/@DifferentialVariationalInequalities/computeStateEquation_Function.m
@@ -2,14 +2,25 @@ function f = computeStateEquation_Function(plant, tau, x, p)
 %computeStateEquation_Function
 %   compute State Equation Function
 
-if plant.computeInvM
-    q_Dim = round(1/2 * plant.Dim.x);
-    M = autoGen_M(x);
-    H = autoGen_H(tau, x, p);
-    f = [x(q_Dim + 1 : end);...
-         M\H];   
-else
-    f = autoGen_f(tau, x, p);   
+switch plant.computeStateEquationMethod
+    case 1
+        f = autoGen_f(tau, x, p); 
+        
+    case 2
+        q_Dim = round(1/2 * plant.Dim.x);
+        M = autoGen_M(x);
+        H = autoGen_H(tau, x, p);
+        f = [x(q_Dim + 1 : q_Dim * 2);...
+            M\H];
+        
+    case 3
+        q_Dim = plant.qDim;
+        M = autoGen_M(x);
+        H = autoGen_H(tau, x, p);  
+        f_given = autoGen_f(tau, x, p);
+        f = [x(q_Dim + 1 : q_Dim * 2);...
+            M\H;...
+            f_given];
 end
 
 end
